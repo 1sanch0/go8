55bc3ddaa4ce038d005a267d4af3923e5cba54d3
SonicHaveJar <javier.sancho.olano@gmail.com>
2024-09-05 20:35:39 +0200

    tick

--- interpreter.go
+++ interpreter.go
@@ -2,7 +2,9 @@ package main
 
 import "math/rand/v2"
 
-const MEM_SIZE = 0x1000;
+const WIDTH = 64
+const HEIGHT = 64
+const MEM_SIZE = 0x1000
 const FONTS_ADDR uint16 = 0x0 // The memory address at which this data resides is unspecified but it's usually lower that 0x200
 var FONT_SPRITES = []uint8{
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -24,30 +26,104 @@ var FONT_SPRITES = []uint8{
 }
 
 type Chip8 struct {
-	V [16]uint8    // Registers
-	I    uint16    // Address register
+	V [16]uint8     // Registers
+	I    uint16     // Address register
 
-  PC   uint16    // Program counter
-  SP   uint16    // Stack pointer
+  PC   uint16     // Program counter
+  SP   uint16     // Stack pointer
 
   dtimer uint8    // Delay timer register
   stimer uint8    // Sound timer register
 
-	memory []uint8 // Memory
+	memory []uint8  // Memory
+
+  display []uint8 // Display
 }
 
-func Chip8_init() (chip8 Chip8) {
+func Chip8_create() (chip8 Chip8) {
 	chip8 = Chip8{}
 	chip8.memory = make([]uint8, MEM_SIZE)
-
-  chip8.PC = 0x200;
-  chip8.SP = MEM_SIZE - 1;
+  chip8.display = make([]uint8, WIDTH * HEIGHT)
 
   for i := 0; i < len(FONT_SPRITES); i++ {
     chip8.memory[FONTS_ADDR + uint16(i)] = FONT_SPRITES[i]
   }
 
 	return
+}
+
+func Chip8_reset(chip8 *Chip8) {
+  chip8.PC = 0x200;
+  chip8.SP = MEM_SIZE - 1;
+
+  // TODO: Reset
+}
+
+func Chip8_tick(chip8 *Chip8, dt float32) {
+  // Fetch
+  var lsb = uint16(chip8.memory[chip8.PC]); chip8.PC++;
+  var msb = uint16(chip8.memory[chip8.PC]); chip8.PC++;
+  var opcode = int(((lsb << 8) & 0xFF00) | (msb & 0x00FF))
+
+  var x = uint8((opcode >> 8) & 0xF)
+  var y = uint8((opcode >> 4) & 0xF)
+  var n = uint8(opcode & 0xF)
+  var nn = uint8(opcode & 0xFF)
+  var nnn = uint16(opcode & 0x0FFF)
+
+  // Decode & Execute
+  switch {
+    case opcode == 0x00E0: _00E0(chip8)
+    case opcode == 0x00EE: _00EE(chip8)
+    case (0x1000 <= opcode) && (opcode < 0x2000): _1NNN(chip8, nnn)
+    case (0x2000 <= opcode) && (opcode < 0x3000): _2NNN(chip8, nnn)
+    case (0x3000 <= opcode) && (opcode < 0x4000): _3XNN(chip8, x, nn)
+    case (0x4000 <= opcode) && (opcode < 0x5000): _4XNN(chip8, x, nn)
+    case (0x5000 <= opcode) && (opcode < 0x6000): _5XY0(chip8, x, y)
+    case (0x6000 <= opcode) && (opcode < 0x7000): _6XNN(chip8, x, nn)
+    case (0x7000 <= opcode) && (opcode < 0x8000): _7XNN(chip8, x, nn)
+    case (0x8000 <= opcode) && (opcode < 0x9000):
+      switch n {
+        case 0x0: _8XY0(chip8, x, y)
+        case 0x1: _8XY1(chip8, x, y)
+        case 0x2: _8XY2(chip8, x, y)
+        case 0x3: _8XY3(chip8, x, y)
+        case 0x4: _8XY4(chip8, x, y)
+        case 0x5: _8XY5(chip8, x, y)
+        case 0x6: _8XY6(chip8, x, y)
+        case 0x7: _8XY7(chip8, x, y)
+        case 0xE: _8XYE(chip8, x, y)
+        default: // TODO: error of some sorts
+      }
+    case (0x9000 <= opcode) && (opcode < 0xA000): _9XY0(chip8, x, y)
+    case (0xA000 <= opcode) && (opcode < 0xB000): _ANNN(chip8, nnn)
+    case (0xB000 <= opcode) && (opcode < 0xC000): _BNNN(chip8, nnn)
+    case (0xC000 <= opcode) && (opcode < 0xD000): _CXNN(chip8, x, nn)
+    case (0xD000 <= opcode) && (opcode < 0xE000): _DXYN(chip8, x, y, n)
+    case (0xE000 <= opcode) && (opcode < 0xF000):
+      switch nn {
+        case 0x9E: _EX9E(chip8, x)
+        case 0xA1: _EXA1(chip8, x)
+        default: // TODO: error of some sorts
+      }
+    case (0xF000 <= opcode):
+      switch nn {
+        case 0x07: _FX07(chip8, x);
+        case 0x0A: _FX0A(chip8, x);
+        case 0x15: _FX15(chip8, x);
+        case 0x18: _FX18(chip8, x);
+        case 0x1E: _FX1E(chip8, x);
+        case 0x29: _FX29(chip8, x);
+        case 0x33: _FX33(chip8, x);
+        case 0x55: _FX55(chip8, x);
+        case 0x65: _FX65(chip8, x);
+        default: // TODO: error of some sorts
+      }
+    default: // TODO: error of some sorts
+  }
+
+  // Update timers
+  // TODO
 }
 
 // Instructions
@@ -95,8 +171,8 @@ func _CXNN(chip8 *Chip8, x, nn uint8) { chip8.V[x] = uint8(rand.IntN(0x100)) & n
 func _1NNN(chip8 *Chip8, nnn uint16) { chip8.PC = nnn; }
 func _BNNN(chip8 *Chip8, nnn uint16) { chip8.PC = nnn + uint16(chip8.V[0]); }
 func _2NNN(chip8 *Chip8, nnn uint16) {
-  var msb = uint8((chip8.PC >> 8) & 0xFF)
-  var lsb = uint8(chip8.PC & 0xFF)
+  var lsb = uint8((chip8.PC >> 8) & 0xFF)
+  var msb = uint8(chip8.PC & 0xFF)
 
   chip8.memory[chip8.SP] = lsb; chip8.SP++;
   chip8.memory[chip8.SP] = msb; chip8.SP++;
@@ -105,7 +181,7 @@ func _00EE(chip8 *Chip8) {
   var lsb = uint16(chip8.memory[chip8.SP]); chip8.SP--;
   var msb = uint16(chip8.memory[chip8.SP]); chip8.SP--;
 
-  chip8.PC = ((msb << 8) & 0xFF00) | (lsb & 0x00FF)
+  chip8.PC = ((lsb << 8) & 0xFF00) | (msb & 0x00FF)
 }
 func _3XNN(chip8 *Chip8, x, nn uint8) { if chip8.V[x] == nn { chip8.PC += 2 } }
 func _4XNN(chip8 *Chip8, x, nn uint8) { if chip8.V[x] != nn { chip8.PC += 2 } }
@@ -127,8 +203,31 @@ func _EXA1(chip8 *Chip8, x uint8) {
 func _ANNN(chip8 *Chip8, nnn uint16) { chip8.I = nnn }
 func _FX1E(chip8 *Chip8, x uint8) { chip8.I += uint16(x) }
 func _DXYN(chip8 *Chip8, x, y, n uint8) {
-  // TODO: Draw a sprite at position VX, VY with N bytes of sprite data starting at the address stored in I 
-  // Set VF to 01 if any set pixels are changed to unset, and 00 otherwise
+  var vx, vy = chip8.V[x], chip8.V[y]
+  var xPos, yPos, data uint8
+
+  chip8.V[0xF] = 0
+  for i := uint8(0); i < n; i++ {
+    data = chip8.memory[chip8.I + uint16(i)]
+    yPos = (vy + i) % HEIGHT
+    for j := uint8(8); j >= 0; j-- {
+      xPos = (vx + j) % WIDTH
+
+      Spx := ((data >> j) & 0x1)                // Sprite pixel
+      Dpx := chip8.display[yPos * WIDTH + xPos] // Display pixel
+
+      px := Spx ^ Dpx                           // New pixel
+
+      if (Dpx == 1 && Dpx == 0) { chip8.V[0xF] = 1 }
+
+      chip8.display[yPos * WIDTH + xPos] = px;
+    }
+  }
+}
+func _00E0(chip8 *Chip8) {
+  for i := 0; i < WIDTH * HEIGHT; i++ {
+    chip8.display[i] = 0;
+  }
 }
 func _FX29(chip8 *Chip8, x uint8) { chip8.I = 5 * uint16(chip8.V[x]) + FONTS_ADDR; }
 func _FX33(chip8 *Chip8, x uint8) {
--- main.go
+++ main.go
@@ -1,31 +1,29 @@
 package main
 
-//import rl "github.com/gen2brain/raylib-go/raylib"
-import "fmt"
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-  var chip8 = Chip8_init()
-
-  _6XNN(&chip8, 0, 69)
-  fmt.Println(chip8.V)
+  var chip8 = Chip8_create()
+  Chip8_reset(&chip8)
 
   // NOTES:
   // - The delay timer counts down perpetually @ 60Hz
   // - The sound timer counts down perpetually @ 60Hz
   //   - The minimum value the timer will respond is 02 (TODO)
 
+	rl.InitWindow(800, 450, "go8 - a simple chip8 interperter in go")
+	defer rl.CloseWindow()
 
-	//rl.InitWindow(800, 450, "raylib [core] example - basic window")
-	//defer rl.CloseWindow()
+	rl.SetTargetFPS(60)
 
-	//rl.SetTargetFPS(60)
+	for !rl.WindowShouldClose() {
+    dt := rl.GetFrameTime()
 
-	//for !rl.WindowShouldClose() {
-	//	rl.BeginDrawing()
+    Chip8_tick(&chip8, dt)
 
-	//	rl.ClearBackground(rl.RayWhite)
-	//	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
-	//	rl.EndDrawing()
-	//}
+		rl.BeginDrawing()
+      rl.ClearBackground(rl.RayWhite)
+		  rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+		rl.EndDrawing()
+	}
 }
